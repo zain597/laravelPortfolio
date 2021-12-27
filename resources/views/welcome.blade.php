46d8c6a56626e06d49f478569ca22135f05f0624
zain597 <67180416+zain597@users.noreply.github.com>
2021-12-27 02:34:55 -0800

    testing123

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -180,7 +180,7 @@
             <div class="contact-content">
                 <div class="column left">
                     <div class="text">Get in touch</div>
-                    <p>Contact us via Email, Whatsapp or directly through this application. Forgive me If I responsed late!</p>
+                    <p>Contact us via Email, Whatsapp or directly through this application. Forgive me If I responed late!</p>
                     <div class="icons">
                         <div class="row">
                             <i class="fa fa-user"></i>
